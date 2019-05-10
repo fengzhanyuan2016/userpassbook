2529981cef6db30aa8a42e3f9495f81f0b57c6dc
FengZhanyuan <Zhanyuan.Feng@partner.bmw.com>
2019-05-10 18:12:30 +0800

    init

--- src/main/java/com/bmw/passbook/service/impl/UserPassService.java
+++ src/main/java/com/bmw/passbook/service/impl/UserPassService.java
@@ -1,16 +1,23 @@
 package com.bmw.passbook.service.impl;
 
 import com.bmw.passbook.constant.Constants;
+import com.bmw.passbook.constant.PassStatus;
 import com.bmw.passbook.dto.Pass;
 import com.bmw.passbook.dto.PassTemplate;
 import com.bmw.passbook.dto.Response;
+import com.bmw.passbook.entity.Merchant;
 import com.bmw.passbook.mapper.PassTemplateRowMapper;
+import com.bmw.passbook.repository.MerchantRepository;
 import com.bmw.passbook.service.IUserPassService;
 import com.bmw.passbook.utils.HBaseUtil;
 import lombok.var;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.PrefixFilter;
+import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
@@ -22,6 +29,12 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class UserPassService implements IUserPassService {
+
+    private MerchantRepository _merchantRepository;
+    public UserPassService(MerchantRepository merchantRepository){
+        _merchantRepository = merchantRepository;
+    }
+
     @Override
     public Response getUserPassInfo(Long userId) throws Exception {
         return null;
@@ -41,6 +54,29 @@ public class UserPassService implements IUserPassService {
     public Response userUsePass(Pass pass) {
         return null;
     }
+
+    /**
+    * 根据优惠卷状态获取优惠卷信息
+    * */
+    private Response getPassInfoByStatus(Long userId, PassStatus status){
+        var rowPrefix = new StringBuilder(String.valueOf(userId)).reverse().toString();
+        byte[] b_rowPrefix = Bytes.toBytes(rowPrefix);
+        CompareFilter.CompareOp compareOption = status == PassStatus.UNUSED?CompareFilter.CompareOp.EQUAL:CompareFilter.CompareOp.NOT_EQUAL;
+        Scan scan = new Scan();
+        //获取特定用户的优惠卷
+        scan.setFilter(new PrefixFilter(b_rowPrefix));
+
+        //找到未使用的优惠卷
+        if(status != PassStatus.ALL){
+            scan.setFilter(new SingleColumnValueFilter(Constants.PassTable.FAMILY_I.getBytes(),Constants.PassTable.CON_DATE.getBytes(),compareOption,Bytes.toBytes("-1")));
+        }
+
+
+
+        return null;
+    }
+
+
     private Map<String, PassTemplate> buildPassTemplateMap(List<Pass> passes) throws IOException, ParseException {
         String[] patterns = new String[]{"yyyy-MM-dd"};
         List<String> templateIds = passes.stream().map(
@@ -63,6 +99,16 @@ public class UserPassService implements IUserPassService {
         }
         return templateId2Object;
     }
+
+    private Map<Integer,Merchant> buildMerchantMap(List<PassTemplate> passTemplates){
+        Map<Integer,Merchant> map = new HashMap<>();
+        var merchantIds = passTemplates.stream().map(PassTemplate::getId).collect(Collectors.toList());
+        var merchants = _merchantRepository.findAllById(merchantIds);
+        merchants.forEach(m->map.put(m.getId(),m));
+        return map;
+    }
+
+
 
 
 
